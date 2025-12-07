87db698df9cca8ec85e39e174c937a4a0c1a6e34
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-12-07 15:45:00 +0600

    chore(vite): add plugin to generate 404 page for React Router on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
+import { readFile, writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
 import { defineConfig, loadEnv } from "vite";
 
 const env = loadEnv("mock", process.cwd(), "");
@@ -8,7 +10,24 @@ const basename = String(env.VITE_BASENAME || "/").replace(/(\/$)|(^\/)/g, "");
 // https://vitejs.dev/config/
 export default defineConfig({
     base: `/${basename}`,
-    plugins: [react()],
+    plugins: [
+        react(),
+
+        //create the 404.html to handle react router for github page
+        {
+            name: "404-html",
+            async writeBundle(options) {
+                const indexHtmlPath = resolve(options.dir || "", "index.html");
+                const buildIndexHtmlPath = resolve(options.dir || "", "404.html");
+                try {
+                    const indexHtmlContent = await readFile(indexHtmlPath, "utf-8");
+                    await writeFile(buildIndexHtmlPath, indexHtmlContent);
+                } catch (error) {
+                    console.error("Error duplicating index.html:", error);
+                }
+            },
+        },
+    ],
     server: {
         port: 3000,
     },
