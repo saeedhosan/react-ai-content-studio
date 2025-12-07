a291f33a11f67e6403a2e60d9a804105921b59a6
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-12-07 15:25:56 +0600

    fix: dynamic VITE_BASENAME in Vite config for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, loadEnv } from "vite";
 
 const env = loadEnv("mock", process.cwd(), "");
 
-const basename = String(env.VITE_BASENAME).replace(/(\/$)|(^\/)/g, "");
+const basename = String(env.VITE_BASENAME || "/").replace(/(\/$)|(^\/)/g, "");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: basename,
+    base: `/${basename}`,
     plugins: [react()],
     server: {
         port: 3000,
